b90805992b359f2a335fb608ebe4d302d2ead933
umeshgandla <umeshkumargandla@gmail.com>
2025-06-18 09:33:54 -0400

    Step 2: Add public subnets

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+output "subnet_ids" {
+  value = aws_subnet.public[*].id
+}
