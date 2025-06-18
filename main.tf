6d18e453a0d35314c6921af999019f2f720e8d6e
umeshgandla <umeshkumargandla@gmail.com>
2025-06-18 09:35:43 -0400

    Step 3: Add Internet Gateway and route

--- main.tf
+++ main.tf
@@ -8,3 +8,22 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
   availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
